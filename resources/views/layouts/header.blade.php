98e982d67c047296c1a016a36d5837be10893952
thetravelsquare <server.thetravelsquare@gmail.com>
2022-06-18 09:37:46 +0530

    link added in header for bookings

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -130,7 +130,7 @@
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Confirm Bookings :</h6>
-                        <a class="collapse-item" href="buttons.html">TTS OTA</a>
+                        <a class="collapse-item" href="{{ route('bookings') }}">TTS OTA</a>
                         <a class="collapse-item" href="cards.html">B2B Fixed Departures</a>
                     </div>
                 </div>
